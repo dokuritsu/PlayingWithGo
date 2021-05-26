bb30a99a95a81fe9624b04a840e2714e5263be67
Laura Pereda <lypereda17@gmail.com>
2021-05-25 19:55:07 -0500

    using rand package

--- lessonTwo/greetings/greetings.go
+++ lessonTwo/greetings/greetings.go
@@ -1,10 +1,45 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
-	// Return a greeting that embeds the name in a message.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+func Hello(name string) (string, error) {
+	// If no name was given, return an error with a message.
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
+	// If a name was received, return a value that embeds the name
+	// in a greeting message.
+	// message := fmt.Sprintf("Hi, %v. Welcome!", name)
+
+	// Create a message with random format
+	message := fmt.Sprintf(randomFormat(), name)
+	return message, nil
+}
+
+// init sets inital values for variables used in the function
+// used this to seed rand package with current time
+// init functions are automatically executed at start up
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// randomFormat returns one of a set of greeting msgs.
+// Returned msg is random
+func randomFormat() string {
+	// slice of msg formats
+	// []string indicates that size of the array is dynamic
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Oh, it's you again, %v",
+		"Hail, %v! Well met!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
--- lessonTwo/hello/hello.go
+++ lessonTwo/hello/hello.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt" // formatting text
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Alejandro")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("Dokuritsu")
+
+	if err != nil {
+		log.Fatal(err) //Fatal = Print equivalent
+	}
+
 	fmt.Println(message)
 }
